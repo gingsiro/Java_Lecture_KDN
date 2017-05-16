505766b672cb32455fe0c4d7dadefc3dd47ac54e
gingsiro <gingsiro@nate.com>
2017-05-16 14:11:37 +0900

    .

--- javase/src/chapter10/ExceptionTest4.java
+++ javase/src/chapter10/ExceptionTest4.java
@@ -1,6 +1,5 @@
 package chapter10;
 
-import java.awt.image.AreaAveragingScaleFilter;
 
 /**
  * - 오류 발생 시키기 : 오류를 강제로 발생시켜 비정상적인 상황에 대한 메세지를 전달하기
@@ -13,6 +12,14 @@ class MyMath {
 		if (j == 0) {
 			// unchecked 이기 때문에 throws 안해도 자동으로 호출한 곳으로 전달 됨
 			throw new ArithmeticException("0으로 나눌 수 없습니다.");
+		} else {
+			return i / j;
+		}
+	}
+	public static int mod(int i, int j) throws Exception{
+		if (j == 0) {
+			// checked 이기 때문에 throws 해줘야 호출한 곳으로 전달 됨
+			throw new Exception("0으로 나눌 수 없습니다.");
 		} else {
 			return i / j;
 		}
---
+++ javase/src/chapter10/ExceptionTest5.java
@@ -0,0 +1,7 @@
+package chapter10;
+
+public class ExceptionTest5 {
+	public static void main(String[] args) {
+		
+	}
+}
